743149b8fd97485f71579d2d4e34bcc172dff304
Brendan Downing <brendanmdowning@gmail.com>
2023-03-26 21:08:08 -0400

    Update

--- src/components/routes/Home.tsx
+++ src/components/routes/Home.tsx
@@ -24,33 +24,36 @@ export const Home = (props: RouteComponentProps<{}> & IHomePageProps) => {
         <Text>
           I'm a <HighlightText color={yellow}>software engineer</HighlightText>,{" "}
           <HighlightText color={yellow}>leader</HighlightText>, and{" "}
-          <HighlightText color={yellow}>dad</HighlightText> in Boston, MA.
+          <HighlightText color={yellow}>dad</HighlightText> based outside of
+          Boston.
         </Text>
         <Text>
-          I'm currently an engineering manager at{" "}
-          <HighlightLink href="https://www.zillowhomeloans.com/">Zillow</HighlightLink>
+          I'm currently a sr. engineering manager at{" "}
+          <HighlightLink href="https://www.zillowhomeloans.com/">
+            Zillow
+          </HighlightLink>
           , building software to improve the consumer mortgage experience.
         </Text>
         <Text>
           Previously, I was a director of engineering at{" "}
-          <HighlightLink href="https://logrocket.com">LogRocket</HighlightLink> and {" "}
-          <HighlightLink href="https://bluestate.co">Blue State Digital</HighlightLink>.{" "}
-          I also co-hosted LogRocket's engineering podcast,{" "}
+          <HighlightLink href="https://logrocket.com">LogRocket</HighlightLink>{" "}
+          and{" "}
+          <HighlightLink href="https://bluestate.co">
+            Blue State Digital
+          </HighlightLink>
+          . I also co-hosted LogRocket's engineering podcast,{" "}
           <HighlightLink href="https://podrocket.logrocket.com/hosts/brendan-downing">
             PodRocket
           </HighlightLink>
           .
         </Text>
         <Text>
-          If you'd like more information on my work, you can find me on{" "}
+          You can find me on{" "}
           <HighlightLink href="https://www.linkedin.com/in/brendanmd/">
             LinkedIn
-          </HighlightLink>, and my Github is {" "}
+          </HighlightLink>
+          , or on Github{" "}
           <HighlightLink href="https://github.com/bmd/">@bmd</HighlightLink>.
-        </Text>
-        <Text>
-          When I'm not writing code, you'll usually find me hanging out with my 2-year-old daughter,
-          or perfecting 
         </Text>
       </section>
       <section>
